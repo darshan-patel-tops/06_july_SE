f6f6eec840d4e1670d9f6be239f8d14efd88a648
darshan <darshan.patel.tops@gmail.com>
2023-07-13 17:17:03 +0530

    first

---
+++ 01.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+//# is a pre processor
+//stdio standard input output
+//.h header file
+//this whole is a library
+
+//in built and user defined
+
+//main is a user defined function
+main()
+{
+
+	/*
+	printf("your statement");
+	
+	*/
+	printf("hello this is  hamza and he is %d",395-25);
+	
+
+}
