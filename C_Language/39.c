7d2af60e131e1a5d395977f2bd5535d69d0592a2
darshan <darshan.patel.tops@gmail.com>
2023-08-08 17:32:28 +0530

    update

---
+++ C_Language/39.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+main()
+{
+	
+	
+	char name[10][10];
+	
+//	for(int i=0;i<1;i++)
+//	{
+		gets(&name[0][10]);
+//		scanf(" %s",&name[i][10]);
+//	 } 
+//	for(int i=0;i<1;i++)
+//	{
+//		printf(" %s",name[i][10]);
+//	 } 
+//	for(int i=0;i<1;i++)
+//	{
+		printf(" %s",name[0][10]);
+//	 } 
+}
